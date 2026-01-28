5faeec4458cfb9a4bcff7752257cfb7d46131f2f
User719-blip <blacviper18@gmail.com>
2026-01-28 18:10:30 +0530

    imidiate patch fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1261738786"
+    serviceWorkerVersion: "2062385522"
   }
 });
